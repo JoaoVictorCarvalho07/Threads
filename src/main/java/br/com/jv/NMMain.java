9a0397671563aef29d1600ffdda876120c1d4657
JoaoVictorCarvalho07 <jvcarvalho018@gmail.com>
2025-11-01 14:17:54 -0300

    threads modelo N:M

---
+++ src/main/java/br/com/jv/NMMain.java
@@ -0,0 +1,49 @@
+package br.com.jv;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class NMMain {
+    public static void main(String[] args) throws InterruptedException {
+        int N = 100; // 100 tarefas
+        int M = 8; // 8 threads
+
+        int iteracoes = 5000000; // 5 milhoes de iteracoes
+
+
+        //aqui o programa executara apenas o numero fixo de threads = 8
+        ExecutorService executor = Executors.newFixedThreadPool(N);
+
+        List<Future<?>> futures = new ArrayList<>(N);
+
+
+        long t0 = System.nanoTime();
+
+        //aqui ele esta adicionando as tarefas ao executor
+        //fila de tarefas a serem executadas com as threads disponivel (8)
+        for (int i = 0; i < N; i++) {
+            futures.add(executor.submit(new TarefaBase(iteracoes)));
+        }
+
+        for (Future<?> f : futures) {
+            try {
+                //aqui ele esta bloqueando a thread atual ate a tarefa terminar
+                //pra nao executar mais de uma tarefa na mesma thread .
+                f.get();
+            } catch (ExecutionException e) {
+                e.getCause().printStackTrace();
+            }
+        }
+
+
+
+        long t1 = System.nanoTime();
+
+        executor.shutdown();
+
+        long tfinal = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
+
+        System.out.printf("N:M | N=%d tarefas, M=%d threads | work=%d | tempo=%d ms%n", N, M, iteracoes, tfinal);
+    }
+}
---
+++ src/main/java/br/com/jv/TarefaBase.java
@@ -0,0 +1,28 @@
+package br.com.jv;
+
+public class TarefaBase implements Runnable {
+    private final int numIteracoes;
+
+    public TarefaBase(int numIteracoes) {
+        this.numIteracoes = numIteracoes;
+    }
+
+    @Override
+    public void run() {
+
+        long acum = 0;
+        for (int i = 0; i < numIteracoes; i++) {
+
+            //nessa operação estamos acumulando a variavel
+            // i*31L multiplica por 31long (forcando uma operacao com long mais custoso)
+            // ^ faz o XOR bit a bit , diferentes vira 1 e iguais vira 0
+            // acum >>> 3 faz com que preencha os bits a esquerda com 0 nao preservando bit de sinal
+            // é uma operação simples mas custosa para base de teste de performace.
+            acum += (i * 31L) ^ (acum >>> 3);
+        }
+
+        // if apenas para o
+        if (acum == 124435) System.out.print("");
+
+    }
+}
