3625b2b97f4b9f7966986fd1603440f005829d89
JoaoVictorCarvalho07 <jvcarvalho018@gmail.com>
2025-11-02 20:44:38 -0300

    chore:tarefa modificada de "acum += (i * 31L) ^ (acum >>> 3)" Para "acum+=i;"

--- src/main/java/br/com/jv/TarefaBase.java
+++ src/main/java/br/com/jv/TarefaBase.java
@@ -1,8 +1,11 @@
 package br.com.jv;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class TarefaBase implements Runnable {
     private final int numIteracoes;
     public static volatile long  uso;
+    public static final AtomicLong CHECK = new AtomicLong();
 
     public TarefaBase(int numIteracoes) {
         this.numIteracoes = numIteracoes;
@@ -15,16 +18,12 @@ public class TarefaBase implements Runnable {
         for (int i = 0; i < numIteracoes; i++) {
 
             //nessa operação estamos acumulando a variavel
-            // i*31L multiplica por 31long (forcando uma operacao com long mais custoso)
-            // ^ faz o XOR bit a bit , diferentes vira 1 e iguais vira 0
-            // acum >>> 3 faz com que preencha os bits a esquerda com 0 nao preservando bit de sinal
-            // é uma operação simples mas custosa para base de teste de performace.
-            acum += (i * 31L) ^ (acum >>> 3);
+            acum+=i;
         }
 
         // if apenas para o
-        if (acum == 124435) System.out.print("");
+        if (acum == 42) System.out.print("");
         uso = acum;
-
+        CHECK.addAndGet(acum);
     }
 }
